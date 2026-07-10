b611b976755c1de07c1ec7ba2ea2e1b17cc6c5c3
abasifrekeiekpo-source <abasifrekeiekpo@gmail.com>
2026-07-10 14:28:05 +0100

    Update greeting message - Abasi-Ifreke Ekpo

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # app.py - Created by Abasi-Ifreke Ekpo
 
 def greet(name):
-    return f"Hello, {name}! Welcome to INFO 52170."
+   return f"Hello, {name}! Welcome back to INFO 52170 GitHub Actions!"
+   
 
 if __name__ == "__main__":
     print(greet("Abasi-Ifreke"))
-    
